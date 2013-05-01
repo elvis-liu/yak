5cef39ee8a38ecca3d47f7885a349125f556e19b
Elvis.Liu <elvisliu.cs@gmail.com>
2013-05-01 21:02:06 +1000

    Trivial fix

--- core/src/test/java/com/thoughtworks/yak/ServiceDefinitionBuilderTest.java
+++ core/src/test/java/com/thoughtworks/yak/ServiceDefinitionBuilderTest.java
@@ -2,7 +2,7 @@ package com.thoughtworks.yak;
 
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class ServiceDefinitionBuilderTest {
     @Test
